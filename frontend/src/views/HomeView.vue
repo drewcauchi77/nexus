df257616ed137f4747522b6f3419ef24fc06e822
Andrew Cauchi <drewcauchi@gmail.com>
2022-12-08 11:26:50 +0100

    Vue setup using CLI instead of Vite

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <h1>Home</h1>
+</template>
+
+<script>
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
